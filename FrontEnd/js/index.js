d22f98f8c7243bfc7ce49a15c8f655ca8416cee9
Necr00 <giannisxristodou@hotmail.com>
2022-10-25 04:52:14 +0300

    Adding DDns Bash script and Usage

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -66,3 +66,68 @@ function drawBackgroundColor() {
       chart.draw(data, options);
     }
     
+
+
+  // Progress Circle Chart
+
+  function makesvg(percentage, inner_text=""){
+
+    var abs_percentage = Math.abs(percentage).toString();
+    var percentage_str = percentage.toString();
+    var classes = ""
+  
+    if(percentage < 0){
+      classes = "danger-stroke circle-chart__circle--negative";
+    } else if(percentage > 0 && percentage <= 30){
+      classes = "warning-stroke";
+    } else{
+      classes = "success-stroke";
+    }
+  
+   var svg = '<svg class="circle-chart" viewbox="0 0 33.83098862 33.83098862" xmlns="http://www.w3.org/2000/svg">'
+       + '<circle class="circle-chart__background" cx="16.9" cy="16.9" r="15.9" />'
+       + '<circle class="circle-chart__circle '+classes+'"'
+       + 'stroke-dasharray="'+ abs_percentage+',100"    cx="16.9" cy="16.9" r="15.9" />'
+       + '<g class="circle-chart__info">'
+       + '   <text class="circle-chart__percent" x="17.9" y="15.5">'+percentage_str+'%</text>';
+  
+    if(inner_text){
+      svg += '<text class="circle-chart__subline" x="16.91549431" y="25">'+inner_text+'</text>'
+    }
+    
+    svg += ' </g></svg>';
+    
+    return svg
+  }
+  
+  (function( $ ) {
+  
+      $.fn.circlechart = function() {
+          this.each(function() {
+              var percentage = $(this).data("percentage");
+              var inner_text = $(this).text();
+              $(this).html(makesvg(percentage, inner_text));
+          });
+          return this;
+      };
+  
+  }( jQuery ));
+
+  $(".circlechart").circlechart(); // Initialization
+
+      var _gaq = _gaq || [];
+      _gaq.push(["_setAccount", "UA-36251023-1"]);
+      _gaq.push(["_setDomainName", "jqueryscript.net"]);
+      _gaq.push(["_trackPageview"]);
+
+      (function () {
+          var ga = document.createElement("script");
+          ga.type = "text/javascript";
+          ga.async = true;
+          ga.src =
+              ("https:" == document.location.protocol
+                  ? "https://ssl"
+                  : "http://www") + ".google-analytics.com/ga.js";
+          var s = document.getElementsByTagName("script")[0];
+          s.parentNode.insertBefore(ga, s);
+      })();
