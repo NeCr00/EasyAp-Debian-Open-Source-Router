ed5433300ec712e034955590587acd388bb34f18
Necr00 <giannisxristodou@hotmail.com>
2023-01-22 21:02:06 +0200

    Finishing Traffic amount and system-changePassword

---
+++ BackEnd/utils/systemConfiguration.js
@@ -0,0 +1,28 @@
+const User = require('../Database/Model/User')
+
+
+async function ChangePassword(reqPassword, reqUsername,reqOldPassword)  {
+
+    let user = await User.findOne({ username: reqUsername, password: reqOldPassword})
+    console.log(user)
+    if (user && user.password !== reqPassword)  {
+        updatePassword = await User.findOneAndUpdate({ username: user.username }, { password: reqPassword }, { new: true })
+        return true
+    }
+    else {
+        return false
+    }
+
+}
+
+
+function resetConfiguration(){
+
+}
+
+function restartServices(){
+    
+}
+
+
+module.exports = { ChangePassword }
