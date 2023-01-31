00151c9259e53175f9d9d5f1efebebccb15d2801
Necr00 <giannisxristodou@hotmail.com>
2023-01-31 15:44:20 +0200

    Updating DataUsage

--- BackEnd/utils/DataUsage/enableDataUsage.js
+++ BackEnd/utils/DataUsage/enableDataUsage.js
@@ -1,14 +1,33 @@
+/*
+This function will apply iptables rules for each IP address that is connected to the access point. The function 
+starts by reading the DHCP leases file, which is typically located at /var/lib/misc/dnsmasq.leases. This file 
+contains information about the IP addresses of devices that have been assigned IP addresses by the DHCP server.
+
+The function then splits the contents of the DHCP leases file by newline characters to get an array of lines. 
+It then iterates through each line, splitting it by spaces and extracting the 3rd element, which is the IP
+address of the connected device.
+
+The function then uses a for loop to iterate through the IP addresses and apply the iptables rules 
+for each device. The iptables -C command is used to check if the rule is already exist or 
+not, if the rule exist it will not apply the rule, but if it does not exist, 
+the iptables -I command is used to insert the rule in the FORWARD chain
+*/
+
+
 const fs = require('fs');
 const exec = require('child_process').exec;
 const dataUsageUser = require('../../Database/Model/DataUsageUser')
 
 function initializeDataUsageForIP(ipv4) {
-
+    //find if the functionality has been enabled already for the specific ip
     findIP = dataUsageUser.findOne({ ip: ipv4 })
     if (findIP) {
         console.log("Ip already initialized")
+        
     }
+
     else {
+        // for a specific ip create 12 entries , configuring each hour for a specific ip
         for (var i = 0; i < 12; i++) {
             createInstanceOfIpPerHour = dataUsageUser.create({
                 ip: ipv4,
@@ -20,6 +39,7 @@ function initializeDataUsageForIP(ipv4) {
             })
         }
     }
+    return
 }
 
 
@@ -37,9 +57,9 @@ function enableDataUsageForIP() {
         var lines = data.split("\n");
         var ips = [];
         for (var i = 0; i < lines.length; i++) {
-            var parts = lines[i].split(" ");
+            var parts = lines[i].split(" "); //split every line's data to an array
             if (parts.length >= 3) {
-                ips.push(parts[2]);
+                ips.push(parts[2]); //get the ip from each line
             }
         }
         let generalStatus={}
@@ -50,6 +70,9 @@ function enableDataUsageForIP() {
             let status = { error: false, message: '' }
             var ip = ips[i];
 
+            //if the rule for network monitor is not applied for the ip, it appends it to the iptable 
+            // if the first command fails, it executes the second, in other words, if the rule doesnt exists then
+            // is added from the  API
             var cmd1 = `iptables -C FORWARD  -d ${ip} -j ACCEPT || iptables -I FORWARD  -d ${ip} -j ACCEPT`
             var cmd2 = `iptables -C FORWARD  -s ${ip} -j ACCEPT || iptables -I FORWARD  -s ${ip} -j ACCEPT`
             // Use exec to run the command
@@ -94,7 +117,7 @@ function enableDataUsageForIP() {
             else{
                 initializeDataUsageForIP(ip)
             }
-                
+                 
         }
         return generalStatus
 
@@ -104,19 +127,3 @@ function enableDataUsageForIP() {
 module.exports = {
 
 }
-
-
-/*
-This function will apply iptables rules for each IP address that is connected to the access point. The function 
-starts by reading the DHCP leases file, which is typically located at /var/lib/misc/dnsmasq.leases. This file 
-contains information about the IP addresses of devices that have been assigned IP addresses by the DHCP server.
-
-The function then splits the contents of the DHCP leases file by newline characters to get an array of lines. 
-It then iterates through each line, splitting it by spaces and extracting the 3rd element, which is the IP
-address of the connected device.
-
-The function then uses a for loop to iterate through the IP addresses and apply the iptables rules 
-for each device. The iptables -C command is used to check if the rule is already exist or 
-not, if the rule exist it will not apply the rule, but if it does not exist, 
-the iptables -I command is used to insert the rule in the FORWARD chain
-*/
--- BackEnd/utils/DataUsage/getClientsUsage.js
+++ BackEnd/utils/DataUsage/getClientsUsage.js
@@ -7,75 +7,96 @@ const dataUsageUser = require('../../Database/Model/DataUsageUser')
 function collectTrafficDataIPs() {
   // Read the dnsmasq lease file
   let leases = fs.readFileSync('/var/lib/misc/dnsmasq.leases', 'utf8');
-  
+
   // Split the leases by newline
   leases = leases.split('\n');
-  
+
   // Iterate through the leases
   for (let lease of leases) {
     // Split the lease by space
     let leaseData = lease.split(' ');
-    
+
     // Extract the IP address, hostname, and timestamp
     let ip_address = leaseData[2];
     let hostname = leaseData[3];
     //let timestamp = new Date(leaseData[0] * 1000);
-    
+
     // Get the device's traffic usage data from iptables
     let trafficData = getTrafficData(ip_address);
+
+    if(trafficData){
+      saveTrafficData(ip_address, trafficData)
+      return true
+    }
+    else{
+      console.log('Cannot save the traffic data for this address: ' + ip_address)
+      return false;
+    }
     
 
-    // Create a new DataUsageUser document
-    let dataUsage = new DataUsageUser({
-      ip_address: ip_address,
-      packets_sent: trafficData.packets_sent,
-      packets_received: trafficData.packets_received,
-      bytes_sent: trafficData.bytes_sent,
-      bytes_received: trafficData.bytes_received,
-      timestamp: timestamp
-    });
-    
-    // Save the DataUsageUser document to the database
-    dataUsage.save(function(err) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(`Data usage for ${ip_address} saved to the database.`);
-      }
-    });
+   
   }
 }
 
 
 
 // schedule the function to run every hour
-setInterval(collectTrafficDataIPs, 1000*60*60);
+setInterval(collectTrafficDataIPs, 1000 * 60 * 60);
+
+
+async function saveTrafficData(ip, currentData) {
+
+  // Find all data for the specific IP
+  const records = await dataUsageUser.find({ ip: ip });
+
+  // Shift all existing data by one hour and update in the database
+  for (let i = records.length - 1; i >= 1; i--) {
+    records[i].packetsSent = records[i - 1].packetsSent;
+    records[i].packetsReceived = records[i - 1].packetsReceived;
+    records[i].bytesSent = records[i - 1].bytesSent;
+    records[i].bytesReceived = records[i - 1].bytesReceived;
+    await records[i].save();
+  }
+
+  // Update the first entry with the current data
+  records[0].packetsSent = data.packetsSent;
+  records[0].packetsReceived = data.packetsReceived;
+  records[0].bytesSent = data.bytesSent;
+  records[0].bytesReceived = data.bytesReceived;
+  await records[0].save();
+}
 
 
 
+async function getTrafficData(ip) {
 
+  enabledIP = await dataUsageUser.find({ ip: ip });
 
-
-  async function getTrafficData(ip) {
+  if (enabledIP) {
     return new Promise((resolve, reject) => {
-        exec(`iptables -L -v -n -x | grep ${ip}`, (err, stdout, stderr) => {
-            if (err) {
-                reject(err);
-            } else {
-                let lines = stdout.split('\n');
-                let data = {};
-                for (let i = 0; i < lines.length; i++) {
-                    let line = lines[i].trim().split(/\s+/);
-                    if (line[0] === '0') {
-                        data.packetsSent = line[1];
-                        data.bytesSent = line[2];
-                    } else if (line[0] === '1') {
-                        data.packetsReceived = line[1];
-                        data.bytesReceived = line[2];
-                    }
-                }
-                resolve(data);
+      exec(`iptables -L -v -n -x | grep ${ip}`, (err, stdout, stderr) => {
+        if (err) {
+          reject(err);
+        } else {
+          let lines = stdout.split('\n');
+          let data = {};
+          for (let i = 0; i < lines.length; i++) {
+            let line = lines[i].trim().split(/\s+/);
+            if (line[0] === '0') {
+              data.packetsSent = line[1];
+              data.bytesSent = line[2];
+            } else if (line[0] === '1') {
+              data.packetsReceived = line[1];
+              data.bytesReceived = line[2];
             }
-        });
+          }
+          resolve(data);
+        }
+      });
     });
+  }
+  else {
+    console.log('There is no an ip address')
+    return false;
+  }
 }
