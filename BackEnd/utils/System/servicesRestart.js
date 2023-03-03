f03d5bf8e17bcdc110335743cc05b093bd09dfbf
Necr00 <giannisxristodou@hotmail.com>
2023-03-04 01:59:39 +0200

    Fixing all except from bind and traffic

--- BackEnd/utils/System/servicesRestart.js
+++ BackEnd/utils/System/servicesRestart.js
@@ -2,76 +2,51 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec)
 const { getVpnStatus } = require('../VPN/configVPN')
 const { getDDnsConfigs } = require('../DNS/ddnsConfig')
+const {initializeTrafficMonitorData} = require('../Dashboard/networkTrafficMonitor')
 
 async function restartServices() {
 
+  try {
     let error = false;
     let errorMessage = "";
 
-    exec('sudo systemctl restart dhcpcd', (err, stdout, stderr) => {
-        if (err) {
-            error = true;
-            errorMessage += `Error restarting dhcpcd: ${err}\n`;
-        }
-        console.log('Done restarting dhcpcd');
-    });
-    exec('sudo systemctl restart dnsmasq', (err, stdout, stderr) => {
-        if (err) {
-            error = true;
-            errorMessage += `Error restarting dnsmasq: ${err}\n`;
-        }
-        console.log('Done restarting dnsmasq');
-    });
+    await exec('sudo systemctl restart dhcpcd');
+    console.log('Done restarting dhcpcd');
 
-    exec('sudo systemctl restart mongodb', (err, stdout, stderr) => {
-        if (err) {
-            error = true;
-            errorMessage += `Error restarting mongodb: ${err}\n`;
-        }
-        console.log('Done restarting mongodb');
-    });
-    exec('sudo systemctl restart hostapd', (err, stdout, stderr) => {
-        if (err) {
-            error = true;
-            errorMessage += `Error restarting hostapd: ${err}\n`;
-        }
-        console.log('Done restarting hostapd');
-    });
+    await exec('sudo systemctl restart dnsmasq');
+    console.log('Done restarting dnsmasq');
 
-    exec('sudo systemctl restart bind9', (err, stdout, stderr) => {
-        if (err) {
-            error = true;
-            errorMessage += `Error restarting bind9: ${err}\n`;
-        }
-        console.log('Done restarting bind9');
-    });
+    await exec('sudo systemctl restart mongod');
+    console.log('Done restarting mongodb');
+
+    await exec('sudo systemctl restart hostapd');
+    console.log('Done restarting hostapd');
+
+    await exec('sudo systemctl restart bind9');
+    console.log('Done restarting bind9');
 
     // if DDNS is used when services restart is initiated
     if (getDDnsConfigs()['ddns_enabled'] === '1') {
-        exec('sudo systemctl restart ddclient', (err, stdout, stderr) => {
-            if (err) {
-                error = true;
-                errorMessage += `Error restarting ddclient: ${err}\n`;
-            }
-            console.log('Done restarting ddclient');
-        });
+      await exec('sudo systemctl restart ddclient');
+      console.log('Done restarting ddclient');
     }
 
     // if user is connected to VPN when services restart is initiated 
     if (await getVpnStatus()['vpn_status'] === 'connected') {
-        exec('sudo systemctl restart openvpn@client', (err, stdout, stderr) => {
-            if (err) {
-                error = true;
-                errorMessage += `Error restarting openvpn@client: ${err}\n`;
-            }
-            console.log('Done restarting openvpn@client');
-        });
+      await exec('sudo systemctl restart openvpn@client');
+      console.log('Done restarting openvpn@client');
     }
+
+
+    initializeTrafficMonitorData()
+    
+
     console.log('Done restarting services');
-    return { error: error, message: errorMessage };
+    return { error: error, message: 'Services has been rebooted' };
+  } catch (error) {
+    console.error(error);
+    return { error: true, message: error.message };
+  }
 }
 
-
-module.exports = {
-    restartServices
-}
+module.exports = { restartServices };
